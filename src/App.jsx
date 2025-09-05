1d32b4c680b1573d53b421e92e4c84f0c262512e
Alef Santos <alef.santos@al.infnet.edu.br>
2025-09-05 17:48:43 -0300

    Refactor App component to remove unused imports and implement card data rendering with Navbar, Sidebar, and Footer components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Card from "./components/Card/Card.jsx";
+import Button from "./components/Button/Button.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Sidebar from "./components/sidebar/Sidebar.jsx";
+
+
+const cardData = [
+  {
+    id: 1,
+    title: "Tópico 1",
+    description: "Uma vista emocionante para se guardar na memória.",
+    imageUrl: "https://picsum.photos/400/250?random=1"
+  },
+  {
+    id: 2,
+    title: "Tópico 2",
+    description: "Descubra belas imagens para se apreciar.",
+    imageUrl: "https://picsum.photos/400/250?random=2"
+  },
+  {
+    id: 3,
+    title: "Tópico 3",
+    description: "Aproveite uma tarde tranquila para ver a beleza da natureza.",
+    imageUrl: "https://picsum.photos/400/250?random=3"
+  },
+  {
+    id: 4,
+    title: "Tópico 4",
+    description: "Observe de perto a beleza natural desta imagem.",
+    imageUrl: "https://picsum.photos/400/250?random=4"
+  },
+  {
+    id: 5,
+    title: "Tópico 5",
+    description: "Uma jornada para apreciar uma verdadeira obra de arte.",
+    imageUrl: "https://picsum.photos/400/250?random=5"
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <Navbar />
+
+      <div className="container">
+        <main className="main">
+
+          <div className="card-item">
+            {cardData.map(({imageUrl, title, description}, i) => (
+              <>
+              <Card key={i} imgUrl={imageUrl} title={title} description={description} />
+              <Button/>
+              </>
+            ))}
+          </div>
+        </main>
+
+        <aside>
+          <Sidebar />
+        </aside>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
